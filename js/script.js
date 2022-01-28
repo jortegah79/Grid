88203d03ab2691ecbfb9b2c97737c3ce533f055a
juan <jortegah79@gmail.com>
2022-01-28 10:51:21 +0100

    añadida animacion

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
- 
+   const inicio=document.getElementById("inicio");
+    saludo=()=>{
+      alert(`
+Bienvenido visitante.
+Estás en una práctica de trabajo con GridLayout!.
+Además este es mi primer evento en js!!`)};
+  inicio.addEventListener("click",saludo);
+   
+   
